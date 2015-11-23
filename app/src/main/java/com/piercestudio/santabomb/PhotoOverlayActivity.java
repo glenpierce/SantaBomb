7f525962637ac1914a052946bd7488b025853c25
Glen Pierce <glen.piercev@gmail.com>
2015-11-22 21:23:41 -0800

    Out of memory error in drawSantaOnImage. santa1.png is a 7MB file.

--- app/src/main/java/com/piercestudio/santabomb/PhotoOverlayActivity.java
+++ app/src/main/java/com/piercestudio/santabomb/PhotoOverlayActivity.java
@@ -3,6 +3,8 @@ package com.piercestudio.santabomb;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,6 +17,8 @@ public class PhotoOverlayActivity extends Activity
 {
 	private String TAG = "photooverlayactivity TAG";
 
+	Bitmap baseImage;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -22,10 +26,11 @@ public class PhotoOverlayActivity extends Activity
 		setContentView(R.layout.photo_overlay_activity);
 
 
-		Bitmap baseImage = getBaseImage();
+		baseImage = getBaseImage();
 
 		ImageView imageView = (ImageView) findViewById(R.id.imageView);
-		imageView.setImageBitmap(baseImage);
+//		imageView.setImageBitmap(baseImage);
+		imageView.setImageBitmap(drawSantaOnImage(baseImage, R.drawable.santa1));
 	}
 
 	private Bitmap getBaseImage(){
@@ -43,5 +48,17 @@ public class PhotoOverlayActivity extends Activity
 			Log.i(TAG, "error retrieving file");
 		}
 		return null;
+	}
+
+	private Bitmap drawSantaOnImage(Bitmap bitmap, int drawableId){
+		Bitmap overlayBitmap = BitmapFactory.decodeResource(this.getResources(), drawableId);
+		Canvas canvas = new Canvas(bitmap);
+//		canvas.drawBitmap(bitmap, 0, 0, null);
+		canvas.drawBitmap(overlayBitmap, 0, 0, null);
+		return bitmap;
+	}
+
+	private BitmapDrawable getBitmapDrawable(){
+		return new BitmapDrawable(getResources(), getBaseImage());
 	}
 }
